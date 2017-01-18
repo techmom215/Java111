5495a0e248953a61fe28d94caf73b8cbf8155cbf
Angela Andrews <techmom215@users.noreply.github.com>
2017-01-17 23:02:18 -0500

    First Commit- Ch1 apps

---
+++ NetBeansProjects/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/* Hello World Application
+    Created for Spring 2016 CSCI 111
+    Last modified January 17, 2017
+    @author Angela Andrews
+*/
+
+package helloworld; //package directive telling the compiler which Java package will contain this software
+
+
+public class HelloWorld { //class header. The class is a public class, meaning that is can be accessed by software from outside the package.
+    /**
+     * main method prints a message on the screen
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        System.out.println("Hello World!");
+        System.out.println("Hello Dr. Kernighan!");
+    }//end main ()
+    
+} //end class HelloWorld
---
+++ NetBeansProjects/businesscard/src/businesscard/Businesscard.java
@@ -0,0 +1,26 @@
+/* Hello World Application
+    Created for Spring 2016 CSCI 111
+    Last modified January 17, 2017
+    @author Angela Andrews
+*/
+package businesscard;
+
+
+public class Businesscard {
+
+    /**
+     * Prints out business card
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Angela ");
+       System.out.println("Andrews");
+       System.out.println("Computer Science major");
+       System.out.println("Community College of Philadelphia");
+       System.out.print("Philadelphia, ");
+       System.out.print("PA ");
+       System.out.println("19130");
+       System.out.println("aandrew1@ccp.edu");
+    } //end main ()
+    
+} //end class Businesscard
